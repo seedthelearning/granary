0d6e4ce02fb236982206652b8ceea44792393497
ADigitalNative <adigitalnative@gmail.com>
2012-07-13 16:03:10 -0400

    Refactored tests

--- spec/api/v1/seeds_controller_spec.rb
+++ spec/api/v1/seeds_controller_spec.rb
@@ -5,24 +5,22 @@ describe "/api/v1/seeds", :type => :api do
   describe "#create" do
     context "seed without a donation" do
       context "seed doesn't exist" do
+        let(:link) { "http//foo.com" }
+
+        before(:each) do
+          seed = FactoryGirl.build(:seed, :link => link)
+          Seed.stub(:create).with(:link => link).and_return(seed)
+          post "api/v1/seeds.json", :body => {:link => link}
+        end
+
         it "returns a 201 Created" do
-          post "api/v1/seeds.json", :body => { :link => "http://foo.com" }
           last_response.status.should == 201
         end
 
         it "returns a json response with the created seed" do
-          link = "http//foo.com"
-          seed = FactoryGirl.build(:seed, :link => link)
-          Seed.stub(:create).with(:link => link).and_return(seed)
-
-          post "api/v1/seeds.json", :body => {:link => link}
           json_response = JSON.parse(last_response.body)
           json_response["link"].should eq(link)
         end
-      end
-
-      context "seed exists" do
-        pending "Not sure how we'd create/identify a duplicate seed"
       end
     end
 
@@ -34,21 +32,22 @@ describe "/api/v1/seeds", :type => :api do
   end
 
   describe "#show" do
-    context "seed exists" do
-      it "returns a 200 OK" do
-        seed = FactoryGirl.build(:seed)
-        Seed.stub(:find).with("1").and_return(seed)
+    let(:url) { "api/v1/seeds/1.json" }
 
-        get "api/v1/seeds/1.json" 
+    context "seed exists" do
+      let(:seed) { FactoryGirl.build(:seed, :link => "http://testtwo.com") }
+      
+      before(:each) do
+        Seed.stub(:find).with("1").and_return(seed)
+        seed.stub(:id).and_return(1)
+        get url
+      end
+
+      it "returns a 200 OK" do
         last_response.status.should == 200
       end
 
       it "returns a json response with the correct seed" do
-        seed = FactoryGirl.build(:seed, :link => "http://testtwo.com")
-        Seed.stub(:find).with("1").and_return(seed)
-        seed.stub(:id).and_return(1)
-
-        get "api/v1/seeds/1.json"
         json_response = JSON.parse(last_response.body)
         json_response["link"].should eq("http://testtwo.com")
         json_response["id"].should eq(1)
@@ -56,21 +55,19 @@ describe "/api/v1/seeds", :type => :api do
     end
 
     context "seed does not exist" do
-      it "returns a 404 not found" do
+      before(:each) do
         Seed.stub(:find).with("1").and_return(nil)
+        get url
+      end
 
-        get "api/v1/seeds/1.json"
+      it "returns a 404 not found" do
         last_response.status.should eq(404)
       end
-      it "returns an error message" do
-        Seed.stub(:find).with("1").and_return(nil)
 
-        get "api/v1/seeds/1.json"
+      it "returns an error message" do
         json_response = JSON.parse(last_response.body)
         json_response["error"].should eq("Seed not found")
       end
     end
   end
 end
-
-class Foo; include Rack::Test::Methods; def app; Rails.application; end; end
